d8b15b896cff0d3e8240de2f586b81135848b7f0
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-15 19:32:14 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Rishi Singh on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
